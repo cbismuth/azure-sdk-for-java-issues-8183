1226be3ff5d88f17dda0b06a11bf796512c3eb80
Christophe Bismuth <christophe.bismuth@gmail.com>
2020-02-24 23:59:58 +0100

    Remove unncessary abstraction

--- src/main/java/fr/tf1/data/monitoring/azure/AzureBlobItemsFinderService.java
+++ src/main/java/fr/tf1/data/monitoring/azure/AzureBlobItemsFinderService.java
@@ -1,22 +1,24 @@
 package fr.tf1.data.monitoring.azure;
 
 import com.azure.core.http.rest.PagedIterable;
+import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.models.BlobContainerItem;
 import com.azure.storage.blob.models.BlobItem;
 import com.microsoft.azure.management.storage.StorageAccount;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Service
 class AzureBlobItemsFinderService {
 
     private final AzureConnector azureConnector;
-    private final AzureDownloadService auditFileDecoderService;
 
-    AzureBlobItemsFinderService(final AzureConnector azureConnector,
-                                final AzureDownloadService auditFileDecoderService) {
+    AzureBlobItemsFinderService(final AzureConnector azureConnector) {
         this.azureConnector = azureConnector;
-        this.auditFileDecoderService = auditFileDecoderService;
     }
 
     void queryStorageAccounts() {
@@ -36,6 +38,28 @@ class AzureBlobItemsFinderService {
 
         final PagedIterable<BlobItem> blobItems = blobContainerClient.listBlobs();
 
-        blobItems.forEach(blobItem -> auditFileDecoderService.downloadToFile(storageAccount, blobContainerItem, blobItem));
+        blobItems.forEach(blobItem -> downloadToFile(storageAccount, blobContainerItem, blobItem));
+    }
+
+    private void downloadToFile(final StorageAccount storageAccount,
+                                final BlobContainerItem blobContainerItem,
+                                final BlobItem blobItem) {
+        final BlobClient blobClient = azureConnector.createBlobClient(storageAccount, blobContainerItem, blobItem.getName());
+
+        final String absolutePath = createTempFile(blobItem);
+
+        blobClient.downloadToFile(absolutePath, true);
+    }
+
+    private String createTempFile(final BlobItem blobItem) {
+        try {
+            final Path tempFile = Files.createTempFile("" + System.currentTimeMillis(), FilenameUtils.getName(blobItem.getName()));
+
+            tempFile.toFile().deleteOnExit();
+
+            return tempFile.toString();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
